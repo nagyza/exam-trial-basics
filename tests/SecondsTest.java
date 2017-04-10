bb16e6a88ee7968625878bf9d047aaf7eac4d0df
Zsolt Nagy <nagyza@gmail.com>
2017-04-10 09:34:23 +0200

    Prepare test class for Seconds class tests

--- tests/SecondsTest.java
+++ tests/SecondsTest.java
@@ -1,10 +1,22 @@
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class SecondsTest {
   @Test
   public void selectSecondElementsTest() throws Exception {
-
+    List<String> testListAll = new ArrayList<>();
+    List<String> testListSeconds = new ArrayList<>();
+    Seconds seconds = new Seconds();
+    for (int i = 0; i < 6; i++) {
+      testListAll.add(Integer.toString(i));
+      if (i % 2 == 0) {
+        testListSeconds.add(Integer.toString(i));
+      }
+    }
+    assertTrue(seconds.selectSecondElements(testListAll) == testListAll);
   }
 }
