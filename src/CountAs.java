dd18754b5940cfcc2ca2d88d6687826d0b706674
Zsolt Nagy <nagyza@gmail.com>
2017-04-10 10:27:29 +0200

    Create a function to read the content of the file.

--- src/CountAs.java
+++ src/CountAs.java
@@ -1,10 +1,28 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CountAs {
   private String fileName;
   private int numberOfAs;
+  private Path filePath;
+  private List<String> lines;
 
   public int countAsInFile(String fileName) {
-
+    if ()
     return this.numberOfAs;
+  }
+
+  public void readFile(String fileName) {
+    this.filePath = Paths.get(fileName);
+    this.lines = new ArrayList<>();
+    try {
+      this.lines = Files.readAllLines(filePath);
+    } catch (IOException e) {
+      this.lines = null;
+    }
   }
 }
