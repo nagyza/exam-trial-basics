1d776cd01d64da9015687c6cb4b5cddd210138d4
Zsolt Nagy <nagyza@gmail.com>
2017-04-10 11:28:13 +0200

    Create Cuboid class with a constructor to initialize all sides

---
+++ src/Cuboid.java
@@ -0,0 +1,12 @@
+
+public class Cuboid {
+  private int sideA;
+  private int sideB;
+  private int sideC;
+
+  public Cuboid(int sideA, int sideB, int sideC) {
+    this.sideA = sideA;
+    this.sideB = sideB;
+    this.sideC = sideC;
+  }
+}
